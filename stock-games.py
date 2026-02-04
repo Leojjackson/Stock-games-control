b8d7cef5aea0ce4132ef87c5bcad55cddc246a0f
Leonardo Jackson <leojjackson@outlook.com>
2026-02-04 20:37:02 -0300

    Adicinado a função que cadastra os jogos novos

--- stock-games.py
+++ stock-games.py
@@ -1,9 +1,25 @@
 stock = {}
-games = []
-
-option = ''
-
 while True:
     print("<<<<- Stock System ->>>>".center(30))
+    print("Escolha uma Opção: ".center(30))
     print("1 -> Adicionar um novo Jogo")
-    print("")
+    print("2 -> Listar todos os jogos")
+    print("3 -> Vender(Remover)Um jogo pelo nome")
+    print("4 -> Sair do programa")
+
+    option = int(input("Digite uma opção: "))
+    if option == 1:
+    # function that saves the datas.
+        def save_data():
+            name = str(input("Nome do jogo: "))
+            price = int(input("Valor do Jogo: "))
+            games = {
+                "Game_name": name,
+                "Game_price": price,
+            }
+            stock.update(games)
+        save_data()
+    if option == 2:
+        print("Os Jogos Cadastrados São: ")
+        print(stock)
+    break
