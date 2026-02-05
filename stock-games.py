5df46b8f383b5b31fff1f42cabdac0bd6da83830
Leonardo Jackson <leojjackson@outlook.com>
2026-02-04 21:11:13 -0300

    Funcionalidades restantes finalizadas.

--- stock-games.py
+++ stock-games.py
@@ -1,5 +1,20 @@
 from time import sleep
-stock = {}
+stock = []
+
+
+def save_data():
+    print('-=' * 15)
+    name = str(input("Nome do jogo: "))
+    price = float(input("Valor do Jogo: "))
+    print('-=' * 15)
+    games = {
+        "Game_name": name,
+        "Game_price": price,
+    }
+    stock.append(games)
+    print(f'Jogo {name} Adicionado com sucesso!')
+    print('-=' * 15)
+
 while True:
     print("<<<<- Stock System ->>>>".center(30))
     print("Escolha uma Opção: ".center(30))
@@ -10,30 +25,29 @@ while True:
 
     option = int(input("Digite uma opção: "))
     if option == 1:
-    # function that saves the datas.
-        def save_data():
-            name = str(input("Nome do jogo: "))
-            price = int(input("Valor do Jogo: "))
-            print('-='*15)
-            games = {
-                "Game_name": name,
-                "Game_price": price,
-            }
-
-            stock.update(games)
         save_data()
     elif option == 2:
+        print('-=' * 15)
         print(f"A quantidade de jogos no estoque é de {len(stock)}")
         print("Os Jogos Cadastrados São: ")
-        print(stock)
+        for game in stock:
+            print(f'Nome: {game['Game_name']} - "Preço: {game["Game_price"]:.2f}"')
+        print('-=' * 15)
     elif option == 3:
-        print(f'Os jogos Cadastrados São: {stock} ')
-        print(f'E {option} Será Removido...')
-        sleep(1)
-        stock.pop("Game_name")
-        print(stock)
+        search = input("Qual jogo deseja remover? ")
+        found = False
+        for game in stock:
+            if game["Game_name"].lower() == search.lower():
+                stock.remove(game)
+                print(f'{game["Game_name"]} Removido com sucesso!')
+                found = True
+                break
+        if not found:
+            print("Jogo não encontrado")
+            print('-=' * 15)
     elif option == 4:
         print("Finalizando o Programa...")
         sleep(1)
         print("Até Mais... Tchau")
+        print('-=' * 15)
         break
